31e75876abf195be987852d440037a3f73d50a8b
ciuches.robert <ciuches.robert@yahoo.com>
2021-04-30 19:41:46 +0300

    custom exception handling

--- src/main/java/com/supportportal/resource/UserResource.java
+++ src/main/java/com/supportportal/resource/UserResource.java
@@ -1,28 +1,57 @@
 package com.supportportal.resource;
 
 import com.supportportal.domain.User;
+import com.supportportal.domain.UserPrincipal;
 import com.supportportal.exceptions.ExceptionHandling;
 import com.supportportal.exceptions.domain.UsernameExistsException;
 import com.supportportal.exceptions.domain.UsernameNotFountException;
 import com.supportportal.service.UserService;
+import com.supportportal.utility.JwtTokenProvider;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
+import static com.supportportal.constant.SecurityConstant.JWT_TOKEN_HEADER;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
-@RequestMapping(path = {"/","/user"})
+@RequestMapping(path = {"/", "/user"})
 public class UserResource extends ExceptionHandling {
 
     private UserService userService;
+    private AuthenticationManager authenticationManager;
+    private JwtTokenProvider jwtTokenProvider;
 
-    public UserResource(UserService userService) {
+    public UserResource(UserService userService, AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+        authenticate(user.getUsername(), user.getPassword());
+        User loginUser = userService.findUserByUsername(user.getUsername());
+        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+        return new ResponseEntity<>(loginUser, jwtHeader, OK);
     }
 
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody User user) throws UsernameNotFountException, UsernameExistsException {
         User newUser = userService.register(user.getFirstName(), user.getLastName(), user.getUsername(), user.getEmail());
         return new ResponseEntity<>(newUser, OK);
+    }
+
+    private HttpHeaders getJwtHeader(UserPrincipal user) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(JWT_TOKEN_HEADER, jwtTokenProvider.generateJwtToken(user));
+        return headers;
+    }
+
+    private void authenticate(String username, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
     }
 }
